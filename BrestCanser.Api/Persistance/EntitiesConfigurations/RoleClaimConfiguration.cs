50d348954f47295789b1da45fbfe4d04af386072
Ahmed Said <ahmeds3id711@gmail.com>
2026-08-04 23:24:37 +0300

    feat(auth): add role permissions

---
+++ BrestCanser.Api/Persistance/EntitiesConfigurations/RoleClaimConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BrestCanser.Api.Persistance.EntitiesConfigurations
+{
+    public class RoleClaimConfiguration : IEntityTypeConfiguration<IdentityRoleClaim<string>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRoleClaim<string>> builder)
+        {
+            var Permission = Permissions.GetAllPermissions();
+            var adminClaims = new List<IdentityRoleClaim<string>>();
+            for (int i = 0; i < Permission.Count; i++)
+            {
+                adminClaims.Add(new IdentityRoleClaim<string>
+                {
+                    Id=i+1,
+                    ClaimType=Permissions.Type,
+                    ClaimValue = Permission[i],
+                    RoleId=DefaultRoles.AdminRoleId
+                });
+            }
+            builder.HasData(adminClaims);
+            
+
+        }
+    }
+}
